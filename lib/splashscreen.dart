078b4249cb8020ca91b7e646ee598510142d2122
Dev Karo <karohunwiashimolowo@gmail.com>
2024-06-25 11:03:30 +0100

    splashscreen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,33 @@
+// lib/splash_screen.dart
+import 'package:flutter/material.dart';
+import 'package:registration_app/register_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToRegister();
+  }
+
+  _navigateToRegister() async {
+    await Future.delayed(Duration(seconds: 3), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => RegisterPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Splash Screen'),
+      ),
+    );
+  }
+}
